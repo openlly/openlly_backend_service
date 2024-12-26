71b31da21034585cc0799d4e3481dd690c50eb13
djsmk123 <djsmk123@gmail.com>
2024-12-26 17:08:09 +0530

    change default port

--- build/api/middleware/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerMiddleware = void 0;
-const loggerMiddleware = (req, res, next) => {
-    req.time = new Date(Date.now()).toString();
-    console.log(req.method, req.hostname, req.path, req.time);
-    next();
-};
-exports.loggerMiddleware = loggerMiddleware;
--- build/redis/redisKeys.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redisKeys = {
-    //user
-    userById: (id) => `user:${id}`,
-    userByUsername: (username) => `user:${username}`,
-};
-exports.default = redisKeys;
